23b98aaab4d0ffa1db1b5a5a7bd26c7f705ebfe7
omargilsturlese <omargilsturlese@S3-01>
2017-03-09 13:58:21 +0100

    push hecho y puntuado a tiempo

--- DynArray/DynArray.cc
+++ DynArray/DynArray.cc
@@ -5,7 +5,7 @@
 DynArray::DynArray(void) {
 	m_capacity=DYN_ARRAY_DEFAULT_SIZE;													
 	m_size= 0;
-	int *m_data=new int[m_capacity];
+	m_data=new int[m_capacity];
 }
 
 DynArray::DynArray(size_t size) {
@@ -16,8 +16,8 @@ DynArray::DynArray(size_t size) {
 
 DynArray::DynArray(size_t size, const int &value) {
 	m_capacity = size;
-	m_size = 0;
-	int *m_data = new int[m_capacity];
+	m_size = size;
+	m_data = new int[size];
 	for (int i = 0; i < m_capacity; i++) {
 		m_data[i] = value;
 	}
@@ -45,15 +45,72 @@ int* begin(void) /*const*/ {
 	return ;
 }
 
-void resize(size_t n) {
-	size_t m_capacity = n;
+void DynArray::resize(size_t n) {
+	m_capacity = n;
 }
 
-void push(const int &val) {
-	size_t m_size = m_size + 1;
-	
+void reserve(size_t n) {
+
 }
+
+void DynArray::shrink(void) {
+	m_capacity = m_size;
+}
+
+bool DynArray::empty(void) const {
+	for (int i = 0; i < m_capacity; i++) {
+		if (m_data[i] != 0) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+}
+
+void DynArray::push(const int &val) {
+	
+	if (m_capacity==m_size) {
+		m_capacity += m_size / 2;
+		int *arr = new int[m_capacity];
+		for (int i = 0; i < m_size; i++) {
+			arr[i] = m_data[i];
+		}
+		delete[] m_data;
+		m_data = arr;
+		
+	}
+	m_data[m_size] = val;
+	m_size += 1;
+}
+
 
 bool operator== (const DynArray& lhs, const DynArray& rhs) {
 	return lhs.m_capacity == rhs.m_capacity && lhs.m_size == rhs.m_size;
+}
+
+bool operator!= (const DynArray& lhs, const DynArray& rhs) {
+	return lhs.m_capacity != rhs.m_capacity || lhs.m_size != rhs.m_size;
+}
+
+bool operator<  (const DynArray& lhs, const DynArray& rhs) {
+	return lhs.m_capacity < rhs.m_capacity;
+}
+
+bool operator<= (const DynArray& lhs, const DynArray& rhs) {
+	return lhs.m_capacity <= rhs.m_capacity;
+}
+
+bool operator>  (const DynArray& lhs, const DynArray& rhs) {
+	return lhs.m_capacity > rhs.m_capacity;
+}
+
+bool operator>= (const DynArray& lhs, const DynArray& rhs) {
+	return lhs.m_capacity >= rhs.m_capacity;
+}
+
+void DynArray::fill(int *first, int *last, int value) {
+	for (int i = *first; i < *last; i++) {
+		m_data[i] = value;
+	}
 }
